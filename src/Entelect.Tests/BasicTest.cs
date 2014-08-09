364fa36212a7e49cc325703f7808914cb941e1cc
Sereph <ryank@entelect.co.za>
2014-08-09 20:41:36 +0200

    Adding a test class with a basic test to confirm CI works.

---
+++ src/Entelect.Tests/BasicTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Entelect.Tests
+{
+    [TestFixture]
+    public class BasicTest
+    {
+         [Test]
+         public void OneEqualsOne()
+         {
+             Assert.AreEqual(1, 1);
+         }
+    }
+}
