dd1d323e75d968f82efdbf9c69052bbad1e9e299
Sereph <ryank@entelect.co.za>
2014-08-09 21:41:24 +0200

    Including nuspec file in project for easy updating.
    Updating assembly info of the project.
    Removing CommonAssemblyInfo.cs, won't be needed in this type of project.
    Adding the MIT licence to the project.

--- src/Entelect/Properties/AssemblyInfo.cs
+++ src/Entelect/Properties/AssemblyInfo.cs
@@ -1,5 +1,18 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Entelect")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Core library used by Entelect. A collection of helper and convenience classes, extension methods and interfaces.")]
 [assembly: Guid("395b550c-c696-490e-b9f1-e53cf827e397")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyCopyright("Copyright © Entelect Software 2011")]
+[assembly: AssemblyProduct("Entelect Library - Core")]
+[assembly: AssemblyCompany("Entelect")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyTrademark("Entelect")]
