a7b146deed85b33bb2b3af362416df9adee117ee
Sereph <ryank@entelect.co.za>
2014-08-13 23:27:44 +0200

    Adding the sandcastle help file builder project.
    Updating git ignore file to exclude the output of the help files.
    Updating the docs for the string builder extension.

--- src/Entelect/Strings/StringBuilderExtensions.cs
+++ src/Entelect/Strings/StringBuilderExtensions.cs
@@ -3,6 +3,9 @@
 // ReSharper disable once CheckNamespace - All extensions are within the same name space otherwise they don't show up in intellisense
 namespace Entelect.Extensions
 {
+    /// <summary>
+    /// A collection of custom extensions for the built in <see cref="System.Text.StringBuilder" /> class.
+    /// </summary>
     public static class StringBuilderExtensions
     {
         /// <summary>
@@ -11,7 +14,7 @@ namespace Entelect.Extensions
         /// <param name="input">The string builder to append to.</param>
         /// <param name="value">The string with the format placeholders.</param>
         /// <param name="arg0">The parameter to use within the formatted string.</param>
-        /// <returns></returns>
+        /// <returns>The StringBuilder with the appended information</returns>
         public static StringBuilder AppendLineFormat(this StringBuilder input, string value, object arg0)
         {
             return input.AppendFormat(value, arg0).AppendLine();
@@ -24,7 +27,7 @@ namespace Entelect.Extensions
         /// <param name="value">The string with the format placeholders.</param>
         /// <param name="arg0">The first parameter to use within the formatted string.</param>
         /// <param name="arg1">The second parameter to use within the formatted string.</param>
-        /// <returns></returns>
+        /// <returns>The StringBuilder with the appended information</returns>
         public static StringBuilder AppendLineFormat(this StringBuilder input, string value, object arg0, object arg1)
         {
             return input.AppendFormat(value, arg0, arg1).AppendLine();
@@ -38,7 +41,7 @@ namespace Entelect.Extensions
         /// <param name="arg0">The first parameter to use within the formatted string.</param>
         /// <param name="arg1">The second parameter to use within the formatted string.</param>
         /// <param name="arg2">The third parameter to use within the formatted string.</param>
-        /// <returns></returns>
+        /// <returns>The StringBuilder with the appended information</returns>
         public static StringBuilder AppendLineFormat(this StringBuilder input, string value, object arg0, object arg1, object arg2)
         {
             return input.AppendFormat(value, arg0, arg1, arg2).AppendLine();
@@ -50,7 +53,7 @@ namespace Entelect.Extensions
         /// <param name="input">The string builder to append to.</param>
         /// <param name="value">The string with the format placeholders.</param>
         /// <param name="args">The parameter to use within the formatted string.</param>
-        /// <returns></returns>
+        /// <returns>The StringBuilder with the appended information</returns>
         public static StringBuilder AppendLineFormat(this StringBuilder input, string value, params object[] args)
         {
             return input.AppendFormat(value, args).AppendLine();
