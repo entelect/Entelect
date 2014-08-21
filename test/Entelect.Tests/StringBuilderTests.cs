ecefc5a6aae8a7dea204b6024a2740643f7af46a
Sereph <ryank@entelect.co.za>
2014-08-21 21:46:44 +0200

    Adding a basic test that uses entelect lib

---
+++ test/Entelect.Tests/StringBuilderTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+using Entelect.Extensions;
+namespace Entelect.Tests
+{
+    [TestFixture]
+    public class StringBuilderTests
+    {
+        [Test]
+        public void AddingIntAndNewline()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLineFormat("SomeStuff{0}", 1);
+            Assert.AreEqual(string.Format("SomeStuff1{0}", Environment.NewLine), sb.ToString());
+        }
+         
+    }
+}
