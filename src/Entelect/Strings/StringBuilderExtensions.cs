cee0be1ad0d05b977ad57c686c2d38c1c76de877
Ryan Kotzen <Ryank@entelect.co.za>
2014-08-27 19:07:50 +0200

    Updating wording, adding exceptions.

--- src/Entelect/Strings/StringBuilderExtensions.cs
+++ src/Entelect/Strings/StringBuilderExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
 // ReSharper disable once CheckNamespace - All extensions are within the same name space otherwise they don't show up in intellisense
 namespace Entelect.Extensions
@@ -12,7 +13,7 @@ namespace Entelect.Extensions
         /// Appends the args and a new line character, basically a shorthand form of input.AppendFormat(...).AppendLine()
         /// </summary>
         /// <param name="input">The string builder to append to.</param>
-        /// <param name="value">The string with the format placeholders.</param>
+        /// <param name="format">The string with the format placeholders.</param>
         /// <param name="arg0">The parameter to use within the formatted string.</param>
         /// <returns>The StringBuilder with the appended information</returns>
         /// <example>  
@@ -22,49 +23,52 @@ namespace Entelect.Extensions
         /// stringBuilder.AppendLineFormat("Hello {0}", "World");
         /// return stringBuilder.ToString(); //Will output Hello World\r\n
         /// </code> 
+        /// <exception cref="T:System.ArgumentNullException"><paramref name="format"/> is null. </exception>
+        /// <exception cref="T:System.FormatException"><paramref name="format"/> is invalid. -or-The index of a format item is less than 0 (zero), or greater than or equal to 1.</exception>
+        /// <exception cref="T:System.ArgumentOutOfRangeException">The length of the expanded string would exceed <see cref="P:System.Text.StringBuilder.MaxCapacity"/>. </exception>
         /// </example> 
-        public static StringBuilder AppendLineFormat(this StringBuilder input, string value, object arg0)
+        public static StringBuilder AppendLineFormat(this StringBuilder input, string format, object arg0)
         {
-            return input.AppendFormat(value, arg0).AppendLine();
+            return input.AppendFormat(format, arg0).AppendLine();
         }
 
         /// <summary>
         /// Appends the args and a new line character
         /// </summary>
         /// <param name="input">The string builder to append to.</param>
-        /// <param name="value">The string with the format placeholders.</param>
+        /// <param name="format">The string with the format placeholders.</param>
         /// <param name="arg0">The first parameter to use within the formatted string.</param>
         /// <param name="arg1">The second parameter to use within the formatted string.</param>
         /// <returns>The StringBuilder with the appended information</returns>
-        public static StringBuilder AppendLineFormat(this StringBuilder input, string value, object arg0, object arg1)
+        public static StringBuilder AppendLineFormat(this StringBuilder input, string format, object arg0, object arg1)
         {
-            return input.AppendFormat(value, arg0, arg1).AppendLine();
+            return input.AppendFormat(format, arg0, arg1).AppendLine();
         }
 
         /// <summary>
         /// Appends the args and a new line character
         /// </summary>
         /// <param name="input">The string builder to append to.</param>
-        /// <param name="value">The string with the format placeholders.</param>
+        /// <param name="format">The string with the format placeholders.</param>
         /// <param name="arg0">The first parameter to use within the formatted string.</param>
         /// <param name="arg1">The second parameter to use within the formatted string.</param>
         /// <param name="arg2">The third parameter to use within the formatted string.</param>
         /// <returns>The StringBuilder with the appended information</returns>
-        public static StringBuilder AppendLineFormat(this StringBuilder input, string value, object arg0, object arg1, object arg2)
+        public static StringBuilder AppendLineFormat(this StringBuilder input, string format, object arg0, object arg1, object arg2)
         {
-            return input.AppendFormat(value, arg0, arg1, arg2).AppendLine();
+            return input.AppendFormat(format, arg0, arg1, arg2).AppendLine();
         }
 
         /// <summary>
         /// Appends the args and a new line character
         /// </summary>
         /// <param name="input">The string builder to append to.</param>
-        /// <param name="value">The string with the format placeholders.</param>
+        /// <param name="format">The string with the format placeholders.</param>
         /// <param name="args">The parameter to use within the formatted string.</param>
         /// <returns>The StringBuilder with the appended information</returns>
-        public static StringBuilder AppendLineFormat(this StringBuilder input, string value, params object[] args)
+        public static StringBuilder AppendLineFormat(this StringBuilder input, string format, params object[] args)
         {
-            return input.AppendFormat(value, args).AppendLine();
+            return input.AppendFormat(format, args).AppendLine();
         }
     }
 }
