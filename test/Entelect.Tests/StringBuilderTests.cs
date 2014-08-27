1599d277bb3b75b3195f28302d1adfaa334908f2
Ryan Kotzen <Ryank@entelect.co.za>
2014-08-27 18:46:45 +0200

    Adding some simple tests to see the effect on code coverage in Team City

--- test/Entelect.Tests/StringBuilderTests.cs
+++ test/Entelect.Tests/StringBuilderTests.cs
@@ -14,6 +14,29 @@ namespace Entelect.Tests
             sb.AppendLineFormat("SomeStuff{0}", 1);
             Assert.AreEqual(string.Format("SomeStuff1{0}", Environment.NewLine), sb.ToString());
         }
-         
+
+        [Test]
+        public void AddingIntIntAndNewline()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLineFormat("SomeStuff{0}{1}", 1, 2);
+            Assert.AreEqual(string.Format("SomeStuff12{0}", Environment.NewLine), sb.ToString());
+        }
+
+        [Test]
+        public void AddingIntIntIntAndNewline()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLineFormat("SomeStuff{0}{1}{2}", 1, 2, 3);
+            Assert.AreEqual(string.Format("SomeStuff123{0}", Environment.NewLine), sb.ToString());
+        }
+
+        [Test]
+        public void AddingIntIntIntIntAndNewline()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLineFormat("SomeStuff{0}{1}{2}{3}", 1, 2, 3, 4);
+            Assert.AreEqual(string.Format("SomeStuff1234{0}", Environment.NewLine), sb.ToString());
+        }
     }
 }
