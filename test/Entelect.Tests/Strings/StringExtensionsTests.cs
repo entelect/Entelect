d0cf43fc2efa02d19a60005a4f6347a9ebf6b02c
Pieter Koornhof <pieter@entelect.co.za>
2014-10-24 06:19:23 +0200

    Add tests for PascalCaseToSpacedString extensions

--- test/Entelect.Tests/Strings/StringExtensionsTests.cs
+++ test/Entelect.Tests/Strings/StringExtensionsTests.cs
@@ -12,12 +12,31 @@ namespace Entelect.Tests.Strings
             var doesContain = "asd".Contains("S",StringComparison.OrdinalIgnoreCase);
             Assert.True(doesContain);
         }
+        
         [Test]
         public void ListContainsIgnoringCase()
         {
             var list = new[] {"asd", "qwe", "zxc"};
             var doesContain = list.Contains("ASD", StringComparison.OrdinalIgnoreCase);
             Assert.True(doesContain);
+        }
+
+        [Test]
+        public void PascalCaseToSpacedString()
+        {
+            const string input = "SomePascalCasedString";
+            const string expectedOutput = "Some Pascal Cased String";
+            var actualOutput = input.PascalToSpacedString();
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [Test]
+        public void PascalCaseToSpacedStringArray()
+        {
+            const string input = "SomePascalCasedString";
+            var expectedOutput = new []{"Some", "Pascal", "Cased","String"};
+            var actualOutput = input.PascalToSpacedStringArray();
+            Assert.That(expectedOutput, Is.EquivalentTo(actualOutput));
         }
     }
 }
